3966900708d1ddbf69b13c29f731fb5b9f236ff3
Praseetha-KR <praseetha@hasgeek.com>
2013-08-11 16:55:38 +0530

    color selectors wip

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 $('#resistance-input').bind('input', function(){
 	$('div').text($(this).val());
 });
+
+$('.color-selector li').each(function(){
+	var color = $(this).attr('id');
+	$(this).css('background-color', color);
+});
